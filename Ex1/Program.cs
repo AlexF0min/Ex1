0ba9a56e5c9ded2ee33b57133f210d4fdd58ee9e
Alexandr Fomin <146896388+AlexF0min@users.noreply.github.com>
2025-06-16 11:05:27 +0200

    Initial commit

---
+++ Ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Check whether a given substring is present in the given string.
+string testString = "Rock n roll is good for your soul";
+string testSubstring = "roll is good";
+
+if (testString.Contains("roll is good"))
+{
+    System.Console.WriteLine($"Text {testSubstring} found, substring is present.");
+}
+
+else
+{
+    System.Console.WriteLine($"Unfortunately {testSubstring} is not present in the main string");
+    
+}
